a8b7c04661a8dfd7ed1ad95f4f58bf00b6fc0245
amisevsk <16168279+amisevsk@users.noreply.github.com>
2025-02-25 22:16:53 +0000

    homebrew: update Homebrew Tap Formula for v1.2.2

--- kitops.rb
+++ kitops.rb
@@ -5,28 +5,28 @@ class Kitops < Formula
 
   on_macos do
     on_arm do
-      url "https://github.com/jozu-ai/kitops/releases/download/v1.2.1/kitops-darwin-arm64.tar.gz"
-      sha256 "3156764042302285f932d0650831fda906e9fe49f0707c00db3ff1261f6764ae"
+      url "https://github.com/jozu-ai/kitops/releases/download/v1.2.2/kitops-darwin-arm64.tar.gz"
+      sha256 "3e7f83383ba90f98f8c01b0681f8fba1b9c736eea91bc4c3b7d14e0195cefe7a"
     end
     on_intel do
-      url "https://github.com/jozu-ai/kitops/releases/download/v1.2.1/kitops-darwin-x86_64.tar.gz"
-      sha256 "d21718fc26c7f9640fc6ca5e2b00a31373d8adb4b2084ef028e895be3f97d55b"
+      url "https://github.com/jozu-ai/kitops/releases/download/v1.2.2/kitops-darwin-x86_64.tar.gz"
+      sha256 "f49dbe538ca79379752b0c7328a7ac8ed002ea12a199f08bfb7f83452c647b77"
     end
 
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/jozu-ai/kitops/releases/download/v1.2.1/kitops-linux-arm64.tar.gz"
-      sha256 "a9537497c0b4b06ecc658244259d6e51116b02af4f138301c68fe3407945e7f0"
+      url "https://github.com/jozu-ai/kitops/releases/download/v1.2.2/kitops-linux-arm64.tar.gz"
+      sha256 "3e0fb0a7bec2e1b6b96091b3fd4ab53748ae6ebbf0f22eeb4fb7893f0f70604a"
     end
     on_intel do
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/jozu-ai/kitops/releases/download/v1.2.1/kitops-linux-x86_64.tar.gz"
-        sha256 "dd2d0143636556dc0160a18185638549714df5bafb0275bedd1bf6be8351bfbf"
+        url "https://github.com/jozu-ai/kitops/releases/download/v1.2.2/kitops-linux-x86_64.tar.gz"
+        sha256 "b06bd37a61c81cd20cc987166a8631d0af9d128ef5ae9fe6ededf92845c6ce86"
       else
-        url "https://github.com/jozu-ai/kitops/releases/download/v1.2.1/kitops-linux-i386.tar.gz"
-        sha256 "1297626164439fa6e001f9f0ed68e22fe2ace2d4e07d8c68b6fa3457bda0c4f7"
+        url "https://github.com/jozu-ai/kitops/releases/download/v1.2.2/kitops-linux-i386.tar.gz"
+        sha256 "4a566c3f7d017f59ab109a57ae316dd5107a681621b99d43b79cdf98ffe4892c"
       end
     end
   end
@@ -36,7 +36,7 @@ class Kitops < Formula
   end
 
   test do
-    expected_version = "Version: 1.2.1"
+    expected_version = "Version: 1.2.2"
     actual_version = shell_output("#{bin}/kit version").strip
     assert_match expected_version, actual_version
   end
